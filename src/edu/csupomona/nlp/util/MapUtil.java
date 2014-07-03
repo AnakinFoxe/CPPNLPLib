2362fd1a8e6b456ce1485b65b285fc281f941457
AnakinFoxe <xing.y.hu@gmail.com>
2014-07-03 15:41:47 -0700

    Updated sortByValue() for MapUtil

--- src/edu/csupomona/nlp/util/MapUtil.java
+++ src/edu/csupomona/nlp/util/MapUtil.java
@@ -6,10 +6,45 @@
 
 package edu.csupomona.nlp.util;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author Xing
  */
 public class MapUtil {
     
+    /*
+    * Refer to http://stackoverflow.com/questions/2864840/treemap-sort-by-value
+    * Sort the Map entities according to the values in descendant order.
+    * It is not able to sort HashMap
+    * @param map Value could be integer or float
+    * @return map Sorted map
+    */
+    public static <K, V extends Comparable<? super V>> Map<K, V> 
+        sortByValue( Map<K, V> map )
+    {
+        List<Map.Entry<K, V>> list =
+            new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+        
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+        {
+            public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+            {
+                return (o2.getValue()).compareTo( o1.getValue() );
+            }
+        } );
+
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list)
+        {
+            result.put( entry.getKey(), entry.getValue() );
+        }
+        return result;
+    }
 }
