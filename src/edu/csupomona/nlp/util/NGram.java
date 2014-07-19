141a43e1a7e75369be179219bab87a38ca3e669f
AnakinFoxe <xing.y.hu@gmail.com>
2014-07-18 21:10:56 -0700

    Updated NGram to make it static and easy to change N value.

--- src/edu/csupomona/nlp/util/NGram.java
+++ src/edu/csupomona/nlp/util/NGram.java
@@ -14,32 +14,24 @@ import java.util.List;
  * @author Xing
  */
 public class NGram {
-    private Integer N;
     
-    public NGram(Integer N) {
-        this.N = N;
-    }
-
-    public Integer getN() {
-        return N;
-    }
-
-    public void setN(Integer N) {
-        this.N = N;
+    private NGram() {
     }
     
     /**
      * Extract n-gram from input text and update the HashMap accordingly.
      * Please preprocess the input text before extraction.
-     * @param map        HashMap to be updated
-     * @param words      Array of string type words
+     * @param N         N of n-gram
+     * @param map       HashMap to be updated
+     * @param words     Array of string type words
     */
-    public void updateNGram(HashMap<String, Integer> map, String[] words) 
+    public static void updateNGram(Integer N, 
+            HashMap<String, Integer> map, String[] words) 
             throws NullPointerException {        
         // create n-gram and update the HashMap
-        for (int i = 0; i <= words.length - this.N; i++) {
+        for (int i = 0; i <= words.length - N; i++) {
             String ngram = words[i];
-            for (int idx = 1; idx < this.N; idx++) 
+            for (int idx = 1; idx < N; idx++) 
                 ngram += " " + words[i + idx];
             
             MapUtil.updateHashMap(map, ngram);
@@ -49,15 +41,17 @@ public class NGram {
     /**
      * Extract n-gram from input text and update the HashMap accordingly.
      * Please preprocess the input text before extraction.
-     * @param map        HashMap to be updated
-     * @param words      List of string type words
+     * @param N         N of n-gram
+     * @param map       HashMap to be updated
+     * @param words     List of string type words
     */
-    public void updateNGram(HashMap<String, Integer> map, List<String> words) 
+    public static void updateNGram(Integer N,
+            HashMap<String, Integer> map, List<String> words) 
             throws NullPointerException {        
         // create n-gram and update the HashMap
-        for (int i = 0; i <= words.size() - this.N; i++) {
+        for (int i = 0; i <= words.size() - N; i++) {
             String ngram = words.get(i);
-            for (int idx = 1; idx < this.N; idx++) 
+            for (int idx = 1; idx < N; idx++) 
                 ngram += " " + words.get(i + idx);
             
             MapUtil.updateHashMap(map, ngram);
