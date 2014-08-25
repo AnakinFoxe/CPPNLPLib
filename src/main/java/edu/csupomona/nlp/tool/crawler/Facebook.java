7728ec08700f49dd8ca8278db64caa29ce6a9969
xing <xing.y.hu@gmail.com>
2014-08-24 17:54:27 -0700

    Adding Facebook crawler

---
+++ src/main/java/edu/csupomona/nlp/tool/crawler/Facebook.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.csupomona.nlp.tool.crawler;
+
+/**
+ * Facebook crawler using Facebook4j
+ * @author xing
+ */
+public class Facebook {
+    
+}
