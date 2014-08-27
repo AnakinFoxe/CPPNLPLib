a640b918916234d1f7c79da861f262acb3d47879
xing <xing.y.hu@gmail.com>
2014-08-27 11:14:53 -0700

    Added method for exchange long-lived token

--- src/main/java/edu/csupomona/nlp/tool/crawler/Facebook.java
+++ src/main/java/edu/csupomona/nlp/tool/crawler/Facebook.java
@@ -12,12 +12,18 @@ import facebook4j.FacebookFactory;
 import facebook4j.Like;
 import facebook4j.PagableList;
 import facebook4j.Page;
+import facebook4j.Paging;
+import facebook4j.Paging.Cursors;
 import facebook4j.Post;
+import facebook4j.RawAPIResponse;
 import facebook4j.ResponseList;
+import facebook4j.auth.AccessToken;
 import facebook4j.conf.ConfigurationBuilder;
 import facebook4j.internal.org.json.JSONException;
 import facebook4j.internal.org.json.JSONObject;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,18 +42,53 @@ public class Facebook {
         // read and construct property
         Properties key = new Properties();
         key.load(getClass().getResourceAsStream("/etc/facebook.properties"));
+        String appId = key.getProperty("AppID");
+        String appSecret = key.getProperty("AppSecret");
+        String userToken = key.getProperty("UserToken");
         
         // set authentication key/token
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
-                .setOAuthAppId(key.getProperty("AppID"))
-                .setOAuthAppSecret(key.getProperty("AppSecret"))
-                .setOAuthAccessToken(key.getProperty("myToken"))
+                .setOAuthAppId(appId)
+                .setOAuthAppSecret(appSecret)
+                .setOAuthAccessToken(userToken)
                 .setOAuthPermissions(key.getProperty("Permissions"));
         
         // create Facebook4j instance
         fb_ = new FacebookFactory(cb.build()).getInstance();
+        
+        // obtain long-lived token
+//        AccessToken newToken = refreshToken(appId, appSecret, userToken);
+        
+        // replace token
+//        fb_.setOAuthAccessToken(newToken);        
+    }
+    
+    private AccessToken refreshToken(String appId, String appSecret, String myToken) {
+        Map<String, String> params = new HashMap<>();
+        params.put("client_id", appId);
+        params.put("client_secret", appSecret);
+        params.put("grant_type", "fb_exchange_token");
+        params.put("fb_exchange_token", myToken);
+        
+        try {
+            // request of an exchange for new token
+            RawAPIResponse apiResponse = 
+                    fb_.callGetAPI("/oauth/access_token", params);
+            
+            // set new token for usage
+            AccessToken newToken = new AccessToken(apiResponse.asString());
+            
+            System.out.println("New Token:" + newToken.getToken());
 
+            return newToken;
+        } catch (FacebookException ex) {
+            Logger.getLogger(Facebook.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        
+        // otherwise return the original one
+        return new AccessToken(myToken);
     }
     
     public void search() throws JSONException {
@@ -101,6 +142,10 @@ public class Facebook {
             
             PagableList<Like> likes = post.getLikes();
             System.out.println(likes.getCount());
+            System.out.println(comments.getCount());
+            Paging<Like> pageLikes = likes.getPaging();
+            Cursors cursors = pageLikes.getCursors();
+            
         } catch (FacebookException ex) {
             Logger.getLogger(Facebook.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -111,7 +156,7 @@ public class Facebook {
         
 //        fb.search();
 //        fb.getPosts();
-        fb.getOtherStuff();
+//        fb.getOtherStuff();
     }
 
     
