913fbb775036a1299b83b4e74d86e64c0f5f61fb
xing <xing.y.hu@gmail.com>
2014-08-28 22:58:21 -0700

    Updated

--- src/main/java/edu/csupomona/nlp/tool/crawler/Facebook.java
+++ src/main/java/edu/csupomona/nlp/tool/crawler/Facebook.java
@@ -23,16 +23,17 @@ import facebook4j.conf.ConfigurationBuilder;
 import facebook4j.internal.org.json.JSONException;
 import facebook4j.internal.org.json.JSONObject;
 import java.io.IOException;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Facebook crawler using Facebook4j
+ * Facebook crawler using Facebook4j.
  * @author xing
  */
 public class Facebook {
@@ -40,8 +41,8 @@ public class Facebook {
     // Facebook4j instance
     private final facebook4j.Facebook fb_;
     
-    // time stamp for crawling restriction
-    private final Date startTime = new Date();
+    // time stamp as crawling starting point
+    private final Date startTime;
     
     public Facebook() throws IOException {    
         // read and construct property
@@ -66,21 +67,77 @@ public class Facebook {
 //        AccessToken newToken = refreshToken(appId, appSecret, userToken);
         
         // replace token
-//        fb_.setOAuthAccessToken(newToken);    
+//        fb_.setOAuthAccessToken(newToken);  
         
-        // Setup time stamp for crawling starting point
-        startTime.setYear(110);
-        startTime.setMonth(0);
-        startTime.setDate(1);
-        System.out.println(startTime.toString());
+        // set the default start time
+        // 2007-1-1, 00:00
+        startTime = new Date(107, 0, 1, 0, 0);
     }
     
-    private AccessToken refreshToken(String appId, String appSecret, String myToken) {
+    /**
+     * for storing info of posts and comments.
+     */
+    private class FacebookEntry {
+        String type_;        // P: post, C: comment
+        Date createdTime_;   // time of creation
+        String id_;          // id of the entry from Facebook
+        Integer numLikes_;   // number of likes
+        
+        public FacebookEntry(String type_, Date createdTime_, 
+                String id_, Integer numLikes_) {
+            this.type_ = type_;
+            this.createdTime_ = createdTime_;
+            this.id_ = id_;
+            this.numLikes_ = numLikes_;
+        }
+        
+        public String getType() {
+            return type_;
+        }
+
+        public void setType(String type) {
+            this.type_ = type;
+        }
+
+        public Date getCreatedTime() {
+            return createdTime_;
+        }
+
+        public void setCreatedTime(Date createdTime) {
+            this.createdTime_ = createdTime;
+        }
+
+        public String getId() {
+            return id_;
+        }
+
+        public void setId(String id) {
+            this.id_ = id;
+        }
+
+        public Integer getNumLikes() {
+            return numLikes_;
+        }
+
+        public void setNumLikes(Integer numLikes) {
+            this.numLikes_ = numLikes;
+        }
+    }
+    
+    /**
+     * Request for exchange new long-lived (60 days expiration) access token.
+     * @param appId             APP ID      
+     * @param appSecret         APP Secret
+     * @param userToken         Current User Access Token
+     * @return                  new User Access Token (long-lived)
+     */
+    private AccessToken refreshToken(String appId, String appSecret, 
+            String userToken) {
         Map<String, String> params = new HashMap<>();
         params.put("client_id", appId);
         params.put("client_secret", appSecret);
         params.put("grant_type", "fb_exchange_token");
-        params.put("fb_exchange_token", myToken);
+        params.put("fb_exchange_token", userToken);
         
         try {
             // request of an exchange for new token
@@ -99,69 +156,97 @@ public class Facebook {
         }
         
         // otherwise return the original one
-        return new AccessToken(myToken);
+        return new AccessToken(userToken);
     }
     
-    public void search() throws JSONException {
+    public List<Page> getPages(String keyword, boolean onlyVerified) 
+            throws JSONException {
+        List<Page> fullPages = new ArrayList<>();
         try {
-            ResponseList<Page> results = fb_.searchPages("samsung");
-            for (Page result : results) {
-                String query = "select is_verified from page where page_id=" + result.getId();
-                JSONObject json = fb_.executeFQL(query).getJSONObject(0);
-                boolean isVerified = json.getBoolean("is_verified");
+            // search pages according to keyword
+            ResponseList<Page> pages = fb_.searchPages(keyword);
+            for (Page page : pages) {
+                // skip unverified pages only this flag is on
+                if (onlyVerified) {
+                    // is_verified field is only accessable through FQL
+                    String query = "select is_verified from page where page_id=" 
+                            + page.getId();
+                    JSONObject json = fb_.executeFQL(query).getJSONObject(0);
+                    boolean isVerified = json.getBoolean("is_verified");
                 
-                if (isVerified)
-                    System.out.println(result.getName() + ":" + result.getId() + " <official>");
-                else
-                    System.out.println(result.getName() + ":" + result.getId());
+                    if (!isVerified)
+                        continue;
+                }
+                
+                // retrieve full information of the page 
+                Page fullPage = fb_.getPage(page.getId());
+                
+                fullPages.add(fullPage);
             }
         } catch (FacebookException ex) {
             Logger.getLogger(Facebook.class.getName())
                     .log(Level.SEVERE, null, ex);
         }
+        
+        return fullPages;
     }
     
-    public void getPosts() {
+    public List<Post> getPosts(String pageId) {
+        List<Post> fullPosts = new ArrayList<>();
         try {
             // cursor is the most recommended way from Facebook.
             // but I found offset is quite easy for me to implement.
-            ResponseList<Post> posts = fb_.getFeed("114219621960016",
-                    new Reading().offset(24));
-            int count = 1;
-            for (Post post : posts) {
-                System.out.println(post.getCreatedTime().toString() + ":"
-                        + count + ":"
-                        + " messge:" + post.getMessage()
-                        + " story:" + post.getStory()
-                        + " caption:" + post.getCaption()
-                        + " desc:" + post.getDescription()
-                        + " name:" + post.getName()
-                        + " id:" + post.getId());
-                count++;
+            int offset = 0;
+            ResponseList<Post> posts;
+            while (( posts = fb_.getFeed(pageId, 
+                    new Reading().offset(offset))).size() > 0) {
+                for (Post post : posts) {
+                    // check post creation time
+                    if (post.getCreatedTime().before(startTime)) 
+                        return fullPosts;
+                    
+                    // add post to the list
+                    fullPosts.add(post);
+                }
+                
+                // move offset
+                offset += 25;   // 25 is default size of responselist
             }
             
-            System.out.println(posts.size());
         } catch (FacebookException ex) {
-            Logger.getLogger(Facebook.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Facebook.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
+        
+        return fullPosts;
     }
     
-    public void getOtherStuff() {
+    public void getComments() {
+        List<Comment> fullComments = new ArrayList<>();
         try {
-            Post post = fb_.getPost("114219621960016_737493369632635");
+            Post post = fb_.getPost("114219621960016_737014276347211");
             System.out.println(post.getMessage());
             
-            // TODO: can not get full list also, need to go next page
+            // get first few comments using getComments from post
             PagableList<Comment> comments = post.getComments();
-            for (Comment comment: comments) 
-                System.out.println(comment.getLikeCount().toString() + ":" 
-                        + comment.getMessage());
+            Paging<Comment> paging;
+            do {
+                for (Comment comment: comments)
+                    fullComments.add(comment);
+                
+                paging = comments.getPaging();
+            } while ((paging != null) && 
+                    ((comments = fb_.fetchNext(paging)) != null));
+            for (Comment comment : fullComments) {
+                System.out.println(comment.getMessage());
+            }
+            System.out.println(fullComments.size());
             
-            PagableList<Like> likes = post.getLikes();
-            System.out.println(likes.getCount());
-            System.out.println(comments.getCount());
-            Paging<Like> pageLikes = likes.getPaging();
-            Cursors cursors = pageLikes.getCursors();
+//            PagableList<Like> likes = post.getLikes();
+//            System.out.println(likes.getCount());
+//            System.out.println(comments.getCount());
+//            Paging<Like> pageLikes = likes.getPaging();
+//            Cursors cursors = pageLikes.getCursors();
             
         } catch (FacebookException ex) {
             Logger.getLogger(Facebook.class.getName()).log(Level.SEVERE, null, ex);
@@ -172,8 +257,11 @@ public class Facebook {
         Facebook fb = new Facebook();
         
 //        fb.search();
-//        fb.getPosts();
-        fb.getOtherStuff();
+        List<Post> posts = fb.getPosts("7224956785");
+        for (Post post : posts)
+            System.out.println(post.getCreatedTime().toString() + ": " 
+                            + post.getMessage());
+//        fb.getComments();
     }
 
     
